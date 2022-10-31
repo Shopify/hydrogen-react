1a995a5d5e00ccc27a32d8fbf30c1bc0b568a9d7
Anthony Frehner <frehner@users.noreply.github.com>
2022-10-31 16:11:17 -0600

    add a todo note

--- apps/nextjs/codegen.ts
+++ apps/nextjs/codegen.ts
@@ -5,6 +5,8 @@ const config: CodegenConfig = {
   // a normal app would only need `./node_modules/...` but we're in a monorepo
   schema: '../../node_modules/@shopify/hydrogen-react/storefront.schema.json',
   documents: 'pages/**/*.tsx',
+  // @TODO a chance here to provide the settings we use to generate our Types for the schema here as well,
+  // so that the Custom Scalar definitions we have in hydrogen-ui can be used here as well.
   generates: {
     './gql/': {
       preset: 'client',
