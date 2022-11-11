5a118c61357f938b3be9ed19f2f0671e5e41487b
Matt Seccafien <matt.seccafien@shopify.com>
2022-11-11 12:13:15 +0100

    Update CartLineProvider.test.tsx

--- packages/react/src/CartLineProvider.test.tsx
+++ packages/react/src/CartLineProvider.test.tsx
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/react';
+import {renderHook} from '@testing-library/react';
 import {CartLine} from './storefront-api-types.js';
 import {useCartLine, CartLineProvider} from './CartLineProvider.js';
 import {getCartLineMock} from './CartProvider.test.helpers.js';
@@ -6,19 +6,13 @@ import {getCartLineMock} from './CartProvider.test.helpers.js';
 it('provides a hook to access cart line data', () => {
   const cartLine = getCartLineMock();
 
-  function Data() {
-    const line = useCartLine();
+  const {result} = renderHook(() => useCartLine(), {
+    wrapper: ({children}) => (
+      <CartLineProvider line={cartLine as CartLine}>
+        {children}
+      </CartLineProvider>
+    ),
+  });
 
-    return <div>{JSON.stringify(line)}</div>;
-  }
-
-  const {container} = render(
-    <CartLineProvider line={cartLine as CartLine}>
-      <Data />
-    </CartLineProvider>
-  );
-
-  expect(container.querySelector('div')).toHaveTextContent(
-    JSON.stringify(cartLine)
-  );
+  expect(result.current).toEqual(cartLine);
 });
