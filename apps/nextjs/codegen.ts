0326ea4406b966937715813f40e1ebcba0545a73
Anthony Frehner <frehner@users.noreply.github.com>
2022-11-16 09:15:11 -0700

    Update apps/nextjs/codegen.ts
    Co-authored-by: Ren Chaturvedi <63201605+rennyG@users.noreply.github.com>

--- apps/nextjs/codegen.ts
+++ apps/nextjs/codegen.ts
@@ -4,7 +4,7 @@ import type {CodegenConfig} from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  // a normal app would only need `./node_modules/...` but we're in a monorepo
+  // A normal app would only need `./node_modules/...` but we're in a monorepo
   schema: '../../node_modules/@shopify/hydrogen-react/storefront.schema.json',
   documents: 'pages/**/*.tsx',
   generates: {
