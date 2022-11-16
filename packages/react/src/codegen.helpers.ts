5cfdcbe3360d9009403c796d05864a14331bca20
Anthony Frehner <frehner@users.noreply.github.com>
2022-11-16 09:31:22 -0700

    Update packages/react/src/codegen.helpers.ts
    Co-authored-by: Ren Chaturvedi <63201605+rennyG@users.noreply.github.com>

--- packages/react/src/codegen.helpers.ts
+++ packages/react/src/codegen.helpers.ts
@@ -1,6 +1,6 @@
 /** Meant to be used with GraphQL CodeGen to type the Storefront API's custom scalars correctly */
 export const storefrontApiCustomScalars = {
-  // keep in sync with the definitions in the app/nextjs/codegen.ts please!
+  // Keep in sync with the definitions in the app/nextjs/codegen.ts!
   DateTime: 'string',
   Decimal: 'string',
   HTML: 'string',
