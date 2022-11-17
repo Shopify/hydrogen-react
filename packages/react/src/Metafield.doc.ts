a60db381646aa669bb83d0e4bc4e1c0bc0f6414b
Ren Chaturvedi <ren.chaturvedi@shopify.com>
2022-11-17 10:26:05 -0500

    fix metafield and money

--- packages/react/src/Metafield.doc.ts
+++ packages/react/src/Metafield.doc.ts
@@ -11,8 +11,8 @@ const data: ReferenceEntityTemplateSchema = {
   {
     name: "parseMetafieldValue",
     type: "utility",
-    url; "api/hydrogen/utilities/parsemetafieldvalue"
-  },],
+    url: "api/hydrogen/utilities/parsemetafieldvalue"
+  }],
   description: "The `Metafield` component renders the value of a Storefront API's\n[Metafield object](https://shopify.dev/api/storefront/referenc common-objects/metafield). Relies on the `locale` property of the `useShop()` hook, so it must be a desendent of `<ShopifyProvider/>`.\nRenders a smart default of the Metafield's `value`. For more information, refer to the [Default output](#default-output) section.",
   type: "component",
   defaultExample: {
--- packages/react/src/Money.doc.ts
+++ packages/react/src/Money.doc.ts
@@ -6,6 +6,7 @@ const data: ReferenceEntityTemplateSchema = {
   related: [{
     name: "useMoney",
     type: "hook",
+    url: "api/hydrogen/hooks/primitive/usemoney"
   }],
   description: "The `Money` component renders a string of the Storefront API's[MoneyV2 object](https://shopify.dev/api/storefront/reference/common-objects/moneyv2) according to the `locale` in the [`ShopifyProvider` component](/api/hydrogen/components/global/shopifyprovider).\nThe component outputs a `<div>`. You can [customize this component](https://api/hydrogen/components#customizing-hydrogen-components) using passthrough props.",
   type: "component",
