f733e270abe3045ba7416bb5ee67191194df8372
Alex Harvey <alex.harvey@shopify.com>
2022-12-19 10:12:52 -0400

    update to newer version of generate-docs and update content to React Storefront Kit

--- packages/react/src/Metafield.doc.ts
+++ packages/react/src/Metafield.doc.ts
@@ -1,36 +1,42 @@
-import { ReferenceEntityTemplateSchema } from '@shopify/generate-docs';
+import {ReferenceEntityTemplateSchema} from '@shopify/generate-docs';
 
 const data: ReferenceEntityTemplateSchema = {
   name: 'metafield',
   category: 'components',
-  related: [{
-    name: "parseMetafield",
-    type: "utility",
-    url: "api/hydrogen/utilities/parsemetafield"
-  },
-  {
-    name: "parseMetafieldValue",
-    type: "utility",
-    url: "api/hydrogen/utilities/parsemetafieldvalue"
-  }],
-  description: "The `Metafield` component renders the value of a Storefront API's\n[Metafield object](https://shopify.dev/api/storefront/referenc common-objects/metafield). Relies on the `locale` property of the `useShop()` hook, so it must be a descendent of `<ShopifyProvider/>`.\nRenders a smart default of the Metafield's `value`. For more information, refer to the [Default output](#default-output) section.",
-  type: "component",
+  related: [
+    {
+      name: 'parseMetafield',
+      type: 'utility',
+      url: 'api/hydrogen/utilities/parsemetafield',
+    },
+    {
+      name: 'parseMetafieldValue',
+      type: 'utility',
+      url: 'api/hydrogen/utilities/parsemetafieldvalue',
+    },
+  ],
+  description:
+    "The `Metafield` component renders the value of a Storefront API's\n[Metafield object](https://shopify.dev/api/storefront/reference/common-objects/metafield). Relies on the `locale` property of the `useShop()` hook, so it must be a descendent of `<ShopifyProvider/>`.\nRenders a smart default of the Metafield's `value`. For more information, refer to the [Default output](#default-output) section.",
+  type: 'component',
   defaultExample: {
-    description: "I am the default example",
+    description: 'I am the default example',
     codeblock: {
-      tabs: [{
-        code: './MetafieldExample.tsx',
-        language: 'jsx',
-      }
+      tabs: [
+        {
+          code: './MetafieldExample.tsx',
+          language: 'jsx',
+        },
       ],
-      title: "Example code",
-    }
+      title: 'Example code',
+    },
   },
-  definitions: [{
-    title: "Props",
-    type: "MetafieldProps",
-    description: "interface description"
-  }],
+  definitions: [
+    {
+      title: 'Props',
+      type: 'MetafieldProps',
+      description: 'interface description',
+    },
+  ],
 };
 
 export default data;
