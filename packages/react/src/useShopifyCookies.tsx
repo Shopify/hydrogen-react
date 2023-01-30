229cbeb97cd4e7fdc7ab66d8ef9a15be9829550a
Maxim Shirshin <maxim.shirshin@shopify.com>
2023-01-30 15:20:59 +0100

    Add support for different cookie retention policies for no consent given

--- packages/react/src/useShopifyCookies.tsx
+++ packages/react/src/useShopifyCookies.tsx
@@ -7,10 +7,33 @@ const longTermLength = 60 * 60 * 24 * 360 * 1; // ~1 year expiry
 const shortTermLength = 60 * 30; // 30 mins
 
 /**
+ * If no consent given:
+ * POLICY_NO_COOKIE: Do not set a cookie
+ * POLICY_SESSIONIZED: Set a session cookie
+ * POLICY_SHORT_TERM: Set a short-term cookie
+ */
+
+export type NoConsentCookiePolicy =
+  | 'POLICY_NO_COOKIE'
+  | 'POLICY_SESSIONIZED'
+  | 'POLICY_SHORT_TERM';
+
+export const POLICY_NO_COOKIE: NoConsentCookiePolicy = 'POLICY_NO_COOKIE';
+export const POLICY_SESSIONIZED: NoConsentCookiePolicy = 'POLICY_SESSIONIZED';
+export const POLICY_SHORT_TERM: NoConsentCookiePolicy = 'POLICY_SHORT_TERM';
+export const ALL_POLICIES: Set<NoConsentCookiePolicy> = new Set([
+  POLICY_NO_COOKIE,
+  POLICY_SESSIONIZED,
+  POLICY_SHORT_TERM,
+]);
+
+/**
  * Set user and session cookies and refresh the expiry time
  * @param hasUserConsent - Defaults to false. If hasUserConsent is true, we can set Shopify unique user
- * token cookie from expiry of 30 mins to 1 year
+ * token cookie for 1 year; otherwise, do what policyForNoConsent specifies
  * @param domain - The domain scope of the cookie
+ * @param policyForNoConsent - One of POLICY_NO_COOKIE, POLICY_SESSIONIZED, POLICY_SHORT_TERM:
+ * how to set cookies if no consent given
  * @example
  * ```tsx
  * import {useShopifyCookies} from '@shopify/storefront-kit-react';
@@ -18,9 +41,43 @@ const shortTermLength = 60 * 30; // 30 mins
  * useShopifyCookies(true, 'my-shop.com')
  * ```
  */
-export function useShopifyCookies(hasUserConsent = false, domain = ''): void {
+
+export const DEFAULT_NO_CONSENT_POLICY = POLICY_SHORT_TERM;
+export function useShopifyCookies(
+  hasUserConsent = false,
+  domain = '',
+  policyForNoConsent: NoConsentCookiePolicy = DEFAULT_NO_CONSENT_POLICY
+): void {
   useEffect(() => {
+    let policy: NoConsentCookiePolicy = policyForNoConsent;
+    /**
+     * Even though TypeScript wouldn't allow this, the library may end up
+     * being called in a non-TS world, and we have to fall back to the
+     * compliant choice.
+     */
+
+    if (!ALL_POLICIES.has(policyForNoConsent)) {
+      policy = DEFAULT_NO_CONSENT_POLICY;
+    }
+
+    /**
+     * Choose how to deal with no consent given.
+     * This is done with the assumption we DON'T get consent.
+     */
+    if (!hasUserConsent && policy === POLICY_NO_COOKIE) {
+      deleteCookie(SHOPIFY_Y, domain);
+      deleteCookie(SHOPIFY_S, domain);
+      return;
+    }
+
     const cookies = getShopifyCookies(document.cookie);
+
+    let maxage: number | undefined;
+    if (policy === POLICY_SHORT_TERM) {
+      maxage = shortTermLength;
+    } else if (policy === POLICY_SESSIONIZED) {
+      maxage = undefined;
+    }
 
     /**
      * Set user and session cookies and refresh the expiry time
@@ -28,25 +85,34 @@ export function useShopifyCookies(hasUserConsent = false, domain = ''): void {
     setCookie(
       SHOPIFY_Y,
       cookies[SHOPIFY_Y] || buildUUID(),
-      hasUserConsent ? longTermLength : shortTermLength,
-      domain
+      domain,
+      hasUserConsent ? longTermLength : maxage
     );
     setCookie(
       SHOPIFY_S,
       cookies[SHOPIFY_S] || buildUUID(),
-      shortTermLength,
-      domain
+      domain,
+      hasUserConsent ? shortTermLength : maxage
     );
+  });
+}
+
+function deleteCookie(name: string, domain: string) {
+  document.cookie = stringify(name, '', {
+    domain,
+    path: '/',
+    expires: new Date('1970-01-01T00:00:00.000Z'),
   });
 }
 
 function setCookie(
   name: string,
   value: string,
-  maxage: number,
-  domain: string
+  domain: string,
+  maxage?: number
 ): void {
   document.cookie = stringify(name, value, {
+    // undefined maxage and expires create a session cookie
     maxage,
     domain,
     samesite: 'Lax',
